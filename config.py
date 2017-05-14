a6efc50314cf27b41b0e92d1e1baadf10b3c7bf8
michael <michaelbecker022@gmail.com>
2017-05-14 08:45:40 -0400

    Initial commit

---
+++ config.py
@@ -0,0 +1,7 @@
+class AgentConfig(object):
+    memory_size = 100
+    discount = 0.9
+    random_start = 30
+    batch_size = 32
+    learning_rate = 0.00025
+    learning_rate_decay = 0.96
---
+++ dqn/agent.py
@@ -0,0 +1,11 @@
+
+class Agent(object):
+    def __init__(self, config, environment, sess):
+        self.sess = sess
+        self.env = environment
+
+    def train(self):
+        pass
+
+    def test(self):
+        pass
